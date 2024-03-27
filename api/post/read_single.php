fdff395da27987962b45cd5024eeeba21bbc2b84
valentine OO <varlomxg@gmail.com>
2024-03-27 13:28:32 +0100

    added get by id

--- api/post/read_single.php
+++ api/post/read_single.php
@@ -12,3 +12,22 @@ $db = $database->connect();
 
 // Instantiate blog post object
 $post = new Post($db);
+
+// Get ID
+$post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+// Get post
+$post->read_single();
+
+// create array
+$post_arr = array(
+    'id' => $post->id,
+    'title' => $post->title,
+    'body' => $post->body,
+    'author' => $post->author,
+    'category_id' => $post->category_id,
+    'category_name' => $post->category_name
+);
+
+// Make JSON
+print_r(json_encode($post_arr));
